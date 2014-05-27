cec4ef8052faf9e6783a1234e09691727b339f7b
Matthieu Moreau <matthieu.moreau@hetic.net>
2014-05-27 21:42:52 +0200

    Update planet.js

--- js/views/planet.js
+++ js/views/planet.js
@@ -15,141 +15,114 @@ define([
       console.log(params);
       var compiledTemplate = _.template( PlanetTemplate, params );
       this.$el.html(compiledTemplate);
-      $.getJSON('data/data.json', function(data){
+
+      d3.json('data/data.json', function(data){
+          console.log(data);
+
         var now = new Date(d3.time.year.floor(new Date()));
-              console.log(now);
-              var spacetime = d3.select('.planet');
-              var width = 960,
-                  height = 500,
-                  radius = Math.min(width, height);
+
+        var spacetime = d3.select('.planet');
+        var width = 960,
+            height = 500,
+            radius = Math.min(width, height);
+
+        // Space
+        var svg = spacetime.append("svg")
+          .attr("width", width)
+          .attr("height", height)
+          .append("g")
+          .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+        // Sun
+
+         var sun = {
+          radius : radius*0.075
+        }
+
+        svg.append("circle")
+          .attr("class", "sun")
+          .attr("r", radius*0.075)
+          .style("fill", "rgba(255, 204, 0, 1.0)");
+
+              data.planets[params.planet].distance_px = radius*data.planets[params.planet].solar_system.distance_solar_coef;
+              data.planets[params.planet].size_px = sun.radius*data.planets[params.planet].solar_system.size_coef;
 
               var radii = {
-                "sun": radius / 8,
-                "planetOrbit": radius / 2.5,
-                "planet": radius / 32,
-                "moonOrbit": radius / 16,
-                "moon": radius / 96
+                "earthOrbit": data.planets[params.planet].distance_px,
+                "earth": data.planets[params.planet].size_px ,
               };
 
-              // Space
-              var svg = spacetime.append("svg")
-                .attr("width", width)
-                .attr("height", height)
-                .append("g")
-                  .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-              // Sun
+              // Earth's orbit
               svg.append("circle")
-                .attr("class", "sun")
-                .attr("r", radii.sun)
-                .style("fill", "rgba(255, 204, 0, 1.0)");
-
-              // Planet's orbit
-              svg.append("circle")
-                .attr("class", "planetOrbit")
-                .attr("r", radii.planetOrbit)
+                .attr("class", "earthOrbit")
+                .attr("r", radii.earthOrbit)
                 .style("fill", "none")
                 .style("stroke", "rgba(255, 204, 0, 0.25)");
 
-              // Current position of Planet in its orbit
-              var planetOrbitPosition = d3.svg.arc()
-                .outerRadius(radii.planetOrbit + 1)
-                .innerRadius(radii.planetOrbit - 1)
+              // Current position of Earth in its orbit
+              var earthOrbitPosition = d3.svg.arc()
+                .outerRadius(radii.earthOrbit + 1)
+                .innerRadius(radii.earthOrbit - 1)
                 .startAngle(0)
                 .endAngle(0);
               svg.append("path")
-                .attr("class", "planetOrbitPosition")
-                .attr("d", planetOrbitPosition)
+                .attr("class", "earthOrbitPosition")
+                .attr("d", earthOrbitPosition)
                 .style("fill", "rgba(255, 204, 0, 0.75)");
 
-              // Planet
+              // Earth
               svg.append("circle")
-                .attr("class", "planet")
-                .attr("r", radii.planet)
-                .attr("transform", "translate(0," + -radii.planetOrbit + ")")
+                .attr("class", "earth")
+                .attr("r", radii.earth)
+                .attr("transform", "translate(0," + -radii.earthOrbit + ")")
                 .style("fill", "rgba(113, 170, 255, 1.0)");
 
               // Time of day
               var day = d3.svg.arc()
-                .outerRadius(radii.planet)
+                .outerRadius(radii.earth)
                 .innerRadius(0)
                 .startAngle(0)
                 .endAngle(0);
               svg.append("path")
                 .attr("class", "day")
                 .attr("d", day)
-                .attr("transform", "translate(0," + -radii.planetOrbit + ")")
+                .attr("transform", "translate(0," + -radii.earthOrbit + ")")
                 .style("fill", "rgba(53, 110, 195, 1.0)");
 
-              // Moon's orbit
-              svg.append("circle")
-                .attr("class", "moonOrbit")
-                .attr("r", radii.moonOrbit)
-                .attr("transform", "translate(0," + -radii.planetOrbit + ")")
-                .style("fill", "none")
-                .style("stroke", "rgba(113, 170, 255, 0.25)");
-
-              // Current position of the Moon in its orbit
-              var moonOrbitPosition = d3.svg.arc()
-                .outerRadius(radii.moonOrbit + 1)
-                .innerRadius(radii.moonOrbit - 1)
-                .startAngle(0)
-                .endAngle(0);
-              svg.append("path")
-                .attr("class", "moonOrbitPosition")
-                .attr("d", moonOrbitPosition(now))
-                .attr("transform", "translate(0," + -radii.planetOrbit + ")")
-                .style("fill", "rgba(113, 170, 255, 0.75)");
-
-              // Moon
-              svg.append("circle")
-                .attr("class", "moon")
-                .attr("r", radii.moon)
-                .attr("transform", "translate(0," + (-radii.planetOrbit + -radii.moonOrbit) + ")")
-                .style("fill", "rgba(150, 150, 150, 1.0)");
-
-              // Update the clock every second
-              setInterval(function () {
-                now = new Date();
-                
-                var interpolatePlanetOrbitPosition = d3.interpolate(planetOrbitPosition.endAngle()(), (2 * Math.PI * d3.time.hours(d3.time.year.floor(now), now).length / d3.time.hours(d3.time.year.floor(now), d3.time.year.ceil(now)).length));
-                
-                var interpolateDay = d3.interpolate(day.endAngle()(), (2 * Math.PI * d3.time.seconds(d3.time.day.floor(now), now).length / d3.time.seconds(d3.time.day.floor(now), d3.time.day.ceil(now)).length));
-                
-                var interpolateMoonOrbitPosition = d3.interpolate(moonOrbitPosition.endAngle()(), (2 * Math.PI * d3.time.hours(d3.time.month.floor(now), now).length / d3.time.hours(d3.time.month.floor(now), d3.time.month.ceil(now)).length));
-                
-                d3.transition().tween("orbit", function () {
-                  return function (t) {
-                    // Animate Planet orbit position
-                    d3.select(".planetOrbitPosition").attr("d", planetOrbitPosition.endAngle(interpolatePlanetOrbitPosition(t)));
-
-                    // Transition Planet
-                    d3.select(".planet")
-                      .attr("transform", "translate(" + radii.planetOrbit * Math.sin(interpolatePlanetOrbitPosition(t) - planetOrbitPosition.startAngle()()) + "," + -radii.planetOrbit * Math.cos(interpolatePlanetOrbitPosition(t) - planetOrbitPosition.startAngle()()) + ")");
-
-                    // Animate day
-                    // Transition day
-                    d3.select(".day")
-                      .attr("d", day.endAngle(interpolateDay(t)))
-                      .attr("transform", "translate(" + radii.planetOrbit * Math.sin(interpolatePlanetOrbitPosition(t) - planetOrbitPosition.startAngle()()) + "," + -radii.planetOrbit * Math.cos(interpolatePlanetOrbitPosition(t) - planetOrbitPosition.startAngle()()) + ")");
+        // Update the clock every second
+        setInterval(function () {
+          now = new Date();
+          
+          var interpolateEarthOrbitPosition = d3.interpolate(earthOrbitPosition.endAngle()(), (2 * Math.PI * d3.time.hours(d3.time.year.floor(now), now).length / d3.time.hours(d3.time.year.floor(now), d3.time.year.ceil(now)).length));
+          
+          var interpolateDay = d3.interpolate(day.endAngle()(), (2 * Math.PI * d3.time.seconds(d3.time.day.floor(now), now).length / d3.time.seconds(d3.time.day.floor(now), d3.time.day.ceil(now)).length));
                     
-                    // Transition Moon orbit
-                    d3.select(".moonOrbit")
-                      .attr("transform", "translate(" + radii.planetOrbit * Math.sin(interpolatePlanetOrbitPosition(t) - planetOrbitPosition.startAngle()()) + "," + -radii.planetOrbit * Math.cos(interpolatePlanetOrbitPosition(t) - planetOrbitPosition.startAngle()()) + ")");
+          d3.transition().tween("orbit", function (d, i) {
+            console.log(d);
+            console.log(i);
+            return function (t) {
+              //console.log('tween', t);
+              // Animate Earth orbit position
+              d3.select(".earthOrbitPosition").attr("d", earthOrbitPosition.endAngle(interpolateEarthOrbitPosition(t)));
 
-                    // Animate Moon orbit position
-                    // Transition Moon orbit position
-                    d3.select(".moonOrbitPosition")
-                      .attr("d", moonOrbitPosition.endAngle(interpolateMoonOrbitPosition(t)))
-                      .attr("transform", "translate(" + radii.planetOrbit * Math.sin(interpolatePlanetOrbitPosition(t) - planetOrbitPosition.startAngle()()) + "," + -radii.planetOrbit * Math.cos(interpolatePlanetOrbitPosition(t) - planetOrbitPosition.startAngle()()) + ")");
-                    
-                    // Transition Moon
-                    d3.select(".moon")
-                      .attr("transform", "translate(" + (radii.planetOrbit * Math.sin(interpolatePlanetOrbitPosition(t) - planetOrbitPosition.startAngle()()) + radii.moonOrbit * Math.sin(interpolateMoonOrbitPosition(t) - moonOrbitPosition.startAngle()())) + "," + (-radii.planetOrbit * Math.cos(interpolatePlanetOrbitPosition(t) - planetOrbitPosition.startAngle()()) + -radii.moonOrbit * Math.cos(interpolateMoonOrbitPosition(t) - moonOrbitPosition.startAngle()())) + ")");
-                  };
-                });
-              }, 1000); 
-      });
+              // Transition Earth
+              d3.select(".earth")
+                .attr("transform", "translate(" + radii.earthOrbit * Math.sin(interpolateEarthOrbitPosition(t) - earthOrbitPosition.startAngle()()) + "," + -radii.earthOrbit * Math.cos(interpolateEarthOrbitPosition(t) - earthOrbitPosition.startAngle()()) + ")");
+
+              // Animate day
+              // Transition day
+              d3.select(".day")
+                .attr("d", day.endAngle(interpolateDay(t)))
+                .attr("transform", "translate(" + radii.earthOrbit * Math.sin(interpolateEarthOrbitPosition(t) - earthOrbitPosition.startAngle()()) + "," + -radii.earthOrbit * Math.cos(interpolateEarthOrbitPosition(t) - earthOrbitPosition.startAngle()()) + ")");
+              
+              // Transition Moon orbit
+              d3.select(".moonOrbit")
+                .attr("transform", "translate(" + radii.earthOrbit * Math.sin(interpolateEarthOrbitPosition(t) - earthOrbitPosition.startAngle()()) + "," + -radii.earthOrbit * Math.cos(interpolateEarthOrbitPosition(t) - earthOrbitPosition.startAngle()()) + ")");
+
+            };
+          });
+        }, 1000);
+    });
     }
   });
   return PlanetView;
